028216629d73eba7c8a1e8c775f8877a127de450
Rick Liu <rickliu000@gmail.com>
2024-08-02 23:35:21 -0700

    FIxed entire bot

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #imports required dependicies for discord
 import discord
-from discord.ext import commands
-
 #import the bottoken
-from apikeys import *
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+from collections import deque 
 
 #importing dependices of playing yt songs
-import yt_dlp
-
+import yt_dlp as yt
+load_dotenv()
 #these options configure ffmpeg to process audio and youtube-dl to download the best audio format froma single video
 FFMEG_OPTIONS = {'options':'-vn'}
 YDL_OPTIONS = {'format' : 'bestaudio' , 'noplaylist' : True}
@@ -19,7 +20,7 @@ intents.voice_states = True
 
 #call the bot when ! is used
 client = commands.Bot(command_prefix ='!', intents=intents) 
-
+queue = deque([])
 #bot is ready to receive commands
 @client.event
 async def on_ready(): 
@@ -42,75 +43,93 @@ async def rick(ctx):
 #when an event function detects something it would something else
 @client.event
 async def on_member_join(member):
-    channel = client.get_channel(1268621896358957180)
-    await channel.send("hello")
+    channel = client.get_channel(1269145446052270174)
+    await channel.send(f"Hello, {member.mention}")
 
 @client.event
 async def on_member_remove(member):
-    channel = client.get_channel(1268621896358957180)
-    await channel.send("seeeeyuhhhh")
+    channel = client.get_channel(1269145446052270174)
+    await channel.send(f"Goodbye, {member.mention}")
 
 #if user in a voice chat and uses this command the bot will join
-@client.command(pass_context = True)
+@client.command()
 async def join(ctx):
     if (ctx.author.voice):
-        channel = ctx.message.author.voice.channel
+        channel = ctx.author.voice.channel
         await channel.connect()
     else:
         await ctx.send("get in a voice channel jittleyang")
 
 #makes the bot leave voice chat if it is in one
-@client.command(pass_context = True)
+@client.command()
 async def leave(ctx):
     if(ctx.voice_client):
         await ctx.guild.voice_client.disconnect()
         await ctx.send("i need to futtulatugan else where fonem")
     else:
         await ctx.send("i aint even in a voice chat dumbass")
-
 @client.command()
 async def __init__(self, client):
         self.client = client
         self.queue = []
 
-@client.command()
-async def play(self, ctx, * , search):
+@client.command(pass_context=True)
+async def play(ctx, * , search):
     voice_channel = ctx.author.voice.channel if ctx.author.voice else None
+    
     if not voice_channel:
         return await ctx.send("get in the call dumbass")
     if not ctx.voice_client:
         await voice_channel.connect()
-    
+    voice = ctx.guild.voice_client
     async with ctx.typing():
-        with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info (f"ytsearch:{search}", download = False)
-            if 'entries' in info:
-                info = info['entries'][0]
+        with yt.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(search, download = False)
             url = info['url']
             title = info['title']
-            self.queue.append((url,title))
-            await ctx.send(f'Added to queue: **{title}**')
+        voice.play(discord.FFmpegPCMAudio(url))
+        await ctx.send(f'Now Playing: **{title}**')
+    
     if not ctx.voice_client.is_playing():
-        await self.play_next(ctx)
+        if queue:
+            song = queue.popleft()
+            voice.play(discord.FFmpegPCMAudio(song['url']))
+            await ctx.send(f'Now Playing: **{song['title']}**')
+        else:
+            await ctx.send('There are no more songs left to play')
 
-#this plays the next song that is queued
-    @client.commands
-    async def play_next(self, ctx):
-        if self.queue:
-            url,title - self.queue.pop(0)
-            source = await discord.FFmpegAudio.from_probe(url, **FFMEG_OPTIONS)
-            ctx.voice_client.play(source, after=lambda _:self.client.loop.create_task(self.play_next(ctx)))
-            await ctx.send(f'Now playing **{title}**')
+    #add song to queue
+    @client.command()
+    async def add(ctx, *, search):
+        with yt.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(search, download = False)
+            title = info['title']
+        queue.append(info)
+        await ctx.send(f'Added to queue: **{title}**')
+        
+    #this plays the next song that is queued
+    @client.command()
+    async def play_next(ctx):
+        if queue:
+            song = queue.popleft()
+            voice.play(discord.FFmpegPCMAudio(song['url']))
+            await ctx.send(f'Now playing **{song['title']}**')
         elif not ctx.voice_client.is_playing():
             await ctx.send("play some space panda will ya")
 
-#this th the skip command
-    @client.commands()
+    #this th the skip command
+    @client.command()
     async def skip(ctx):
         if ctx.voice_client and ctx.voice_client.is_playing():
             ctx.voice_client.stop()
             await ctx.send("This song is some ass play the next one")
+            if queue:
+                song = queue.popleft()
+                voice.play(discord.FFmpegPCMAudio(song['url']))
+                await ctx.send(f'Now Playing: **{song['title']}**')
+            else:
+                await ctx.send('There are no more songs left to play')
 
-client.run(bottoken)
+client.run(os.getenv('DISCORD_TOKEN'))
 
 
